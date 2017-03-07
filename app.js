76808a634e63a47ef9166bd81fdb53508137f3b5
Sirfame Lin <sifame@hotmail.com>
2017-03-07 00:07:05 -0800

    debug line on comment to track comment ownership

--- app.js
+++ app.js
@@ -21,13 +21,7 @@ angular.module('flapperNews', ['ui.router'])
 ])
 .factory('posts', [function() {
     var o = {
-      posts: [
-          {title: 'post 1', upvotes: 5},
-          {title: 'post 2', upvotes: 2},
-          {title: 'post 3', upvotes: 15},
-          {title: 'post 4', upvotes: 9},
-          {title: 'post 5', upvotes: 4}
-      ]
+      posts: []
     };
     return o;
 }])
