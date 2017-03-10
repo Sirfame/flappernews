5126d43be46194e13eda4b84cd47554228751999
Sirfame Lin <sifame@hotmail.com>
2017-03-10 12:03:13 -0800

    Finished Securing Endpoints with JWT Authentication

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,15 +1,21 @@
 var express  = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt      = require('express-jwt');
 var Post     = mongoose.model('Post');
 var Comment  = mongoose.model('Comment');
+var User     = mongoose.model('User');
 
 var router = express.Router();
+var auth   = jwt({secret: 'SECRET', userProperty: 'payload'});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Retrieving all posts
 router.get('/posts', function(req, res, next) {
     Post.find(function(err, posts) {
         if(err) {
@@ -19,9 +25,10 @@ router.get('/posts', function(req, res, next) {
     });
 });
 
-router.post('/posts', function(req, res, next) {
+// Posting new posts
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
-
+    post.author = req.payload.username;
     post.save(function(err, post) {
         if(err) {
             return next(err);
@@ -32,7 +39,6 @@ router.post('/posts', function(req, res, next) {
 
 router.param('post', function(req, res, next, id) {
     var query = Post.findById(id);
-
     query.exec(function(err, post) {
         if(err) {
             return next("here - " + err);
@@ -40,12 +46,12 @@ router.param('post', function(req, res, next, id) {
         if(!post) {
             return next(new Error('can\'t find post'));
         }
-
         req.post = post;
         return next();
     });
 });
 
+// Retrieving specific post
 router.get('/posts/:post', function(req, res) {
     req.post.populate('comments', function(err, post) {
         if(err) {
@@ -55,7 +61,8 @@ router.get('/posts/:post', function(req, res) {
     });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+// Upvoting specific posts
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
     req.post.upvote(function(err, post) {
         if(err) {
             return next(err);
@@ -64,10 +71,11 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+// Commenting on specific posts
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
-
+    comment.author = req.payload.username;
     comment.save(function(err, comment) {
         if(err) {
             return next(err)
@@ -83,13 +91,13 @@ router.post('/posts/:post/comments', function(req, res, next) {
     });
 });
 
+// Retrieving a specific comment for a specific post
 router.get('/posts/:post/comments/:comment', function(req, res, next) {
     res.json(req.comment);
 });
 
 router.param('comment', function(req, res, next, id) {
     var query = Comment.findById(id);
-
     query.exec(function(err, comment) {
         if(err) {
             return next(err);
@@ -97,23 +105,58 @@ router.param('comment', function(req, res, next, id) {
         if(!comment) {
             return next(new Error('can\'t find comment'));
         }
-
         req.comment = comment;
         return next();
     });
 });
 
+// Retrieving all comment for a specific post
 router.get('/posts/:post/comments', function(req, res) {
     res.json(req.post.comments);
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+// Upvoting specific a comment
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
     req.comment.upvote(function(err, comment) {
         if(err) {
             return next(err);
         }
         res.json(comment);
     });
+});
+
+// Registering new user
+router.post('/register', function(req, res, next) {
+    if(!req.body.username || !req.body.password) {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    var user = new User();
+    user.username = req.body.username;
+    user.setPassword(req.body.password);
+    user.save(function(err) {
+        if(err) {
+            return next(err);
+        }
+        return res.json({token: user.generateJWT()});
+    });
+});
+
+// Logging in existing user
+router.post('/login', function(req, res, next) {
+    if(!req.body.username || !req.body.password) {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+    passport.authenticate('local', function(err, user, info) {
+        if(err) {
+            return next(err);
+        }
+        if(user) {
+            return res.json({token: user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
