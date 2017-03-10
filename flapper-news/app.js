fdc86cb555d277897fdfbbd14937f367f2050c7b
Sirfame Lin <sifame@hotmail.com>
2017-03-09 23:22:32 -0800

    Finished Setting Up Passport

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,15 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
+var passport     = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+//changed file location
+//changed from ./routes/users.js
+require('./models/Users');
+require('./config/passport');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+//Uncomment here
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -24,9 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
-app.use('/users', users);
+//Uncomment here
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
